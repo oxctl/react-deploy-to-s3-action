7d62717ba26a5b662df0bb3ae06edfa83a43e2c4
Matthew Buckett <buckett@bumph.org>
2024-03-21 09:20:57 +0000

    Don't set the access key id/secret
    This might be why it isn't working with session tokens.

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,8 @@ fi
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 aws configure --profile react-deploy-to-s3-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
+
+
 ${AWS_REGION}
 text
 EOF
